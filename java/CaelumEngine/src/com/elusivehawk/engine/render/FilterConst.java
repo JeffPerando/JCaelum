27b34fde031edba4b7f02ef76a9a3f3ffd391b98
Elusivehawk <be_the_trade@yahoo.com>
2014-08-06 22:17:09 -0700

    Created FilterConst.

---
+++ java/CaelumEngine/src/com/elusivehawk/engine/render/FilterConst.java
@@ -0,0 +1,18 @@
+
+package com.elusivehawk.engine.render;
+
+import java.util.UUID;
+
+/**
+ * 
+ * 
+ * 
+ * @author Elusivehawk
+ */
+public final class FilterConst
+{
+	public static final UUID
+			LIGHTING = UUID.randomUUID(), 
+			COLOR = UUID.randomUUID();
+	
+}
