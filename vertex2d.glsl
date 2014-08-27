7dd8cf72e3401ebe2fbc37713d37dab868b6d338
Elusivehawk <be_the_trade@yahoo.com>
2014-08-26 23:22:22 -0700

    Minor 2D shader revamp; Will work on it more when I'm of a mind to.

--- vertex2d.glsl
+++ vertex2d.glsl
@@ -1,17 +1,16 @@
 #version 330
 
-(location = 0) in vec2 in_position;
-(location = 1) in vec4 in_color;
-(location = 2) in vec2 in_texcoord;
+(location = 0) in vec2 in_pos;
+(location = 1) in vec2 in_tex;
 
-out vec4 frag_color;
+//out vec4 frag_color;
 out vec2 frag_texcoord;
 
 void main()
 {
-	gl_Position = vec4(in_position, 0, 1);
+	gl_Position = vec4(in_pos, 0, 1);
 	
-	frag_color = in_color;
+//	frag_color = in_color;
 	frag_texcoord = in_texcoord;
 	
 }
