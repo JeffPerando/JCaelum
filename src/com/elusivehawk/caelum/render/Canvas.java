c01d86e635d5370ea2c28df9d38de0d4f9a8042d
Elusivehawk <be_the_trade@yahoo.com>
2015-01-09 22:56:13 -0800

    Implemented canvas layering.

--- src/com/elusivehawk/caelum/render/Canvas.java
+++ src/com/elusivehawk/caelum/render/Canvas.java
@@ -1,19 +1,12 @@
 
 package com.elusivehawk.caelum.render;
 
-import java.nio.FloatBuffer;
 import com.elusivehawk.caelum.prefab.Rectangle;
-import com.elusivehawk.caelum.render.gl.GL1;
-import com.elusivehawk.caelum.render.gl.GLBuffer;
-import com.elusivehawk.caelum.render.gl.GLConst;
-import com.elusivehawk.caelum.render.gl.GLEnumBufferTarget;
-import com.elusivehawk.caelum.render.gl.GLEnumDataType;
-import com.elusivehawk.caelum.render.gl.GLEnumDataUsage;
-import com.elusivehawk.caelum.render.gl.GLEnumDrawType;
 import com.elusivehawk.caelum.render.gl.GLProgram;
+import com.elusivehawk.caelum.render.glsl.Shaders;
+import com.elusivehawk.caelum.render.tex.Material;
 import com.elusivehawk.util.IPopulator;
-import com.elusivehawk.util.math.MathHelper;
-import com.elusivehawk.util.storage.BufferHelper;
+import com.elusivehawk.util.storage.Buffer;
 
 /**
  * 
@@ -23,23 +16,18 @@ import com.elusivehawk.util.storage.BufferHelper;
  */
 public class Canvas extends RenderableObj
 {
-	public static final int IMAGE_BUFFER_SIZE = 12;
-	
 	public static final int INDICES_PER_IMG = 6;
 	public static final int FLOATS_PER_INDEX = 4;
 	public static final int FLOATS_PER_IMG = FLOATS_PER_INDEX * INDICES_PER_IMG;
 	
-	private final GLBuffer vertex;
+	private final Buffer<CanvasLayer> layers = new Buffer<CanvasLayer>();
+	private CanvasLayer currentLayer = null;
 	
-	private FloatBuffer imgbuf = null;
-	private Rectangle sub = null;
-	
-	private int images = 0;
-	private boolean expanded = false, updateImgBuf = false, correctCoords = true;
+	private boolean correctCoords = true;
 	
 	public Canvas()
 	{
-		this(new GLProgram(), IMAGE_BUFFER_SIZE);
+		this(1);
 		
 	}
 	
@@ -51,9 +39,51 @@ public class Canvas extends RenderableObj
 		
 	}
 	
+	public Canvas(int layerCount)
+	{
+		this(new GLProgram(), layerCount);
+		
+	}
+	
+	public Canvas(int layerCount, IPopulator<Canvas> pop)
+	{
+		this(layerCount);
+		
+		pop.populate(this);
+		
+	}
+	
+	public Canvas(Shaders shs)
+	{
+		this(shs, 1);
+		
+	}
+	
+	public Canvas(Shaders shs, IPopulator<Canvas> pop)
+	{
+		this(shs);
+		
+		pop.populate(this);
+		
+	}
+	
+	public Canvas(Shaders shs, int layerCount)
+	{
+		this(new GLProgram(shs), layerCount);
+		
+	}
+	
+	public Canvas(Shaders shs, int layerCount, IPopulator<Canvas> pop)
+	{
+		this(shs, layerCount);
+		
+		pop.populate(this);
+		
+	}
+	
 	public Canvas(GLProgram program)
 	{
-		this(program, IMAGE_BUFFER_SIZE);
+		this(program, 1);
 		
 	}
 	
@@ -65,99 +95,77 @@ public class Canvas extends RenderableObj
 		
 	}
 	
-	public Canvas(int images)
-	{
-		this(new GLProgram(), images);
-		
-	}
-	
-	public Canvas(int images, IPopulator<Canvas> pop)
-	{
-		this(images);
-		
-		pop.populate(this);
-		
-	}
-	
 	@SuppressWarnings("unqualified-field-access")
-	public Canvas(GLProgram program, int images)
+	public Canvas(GLProgram p, int layerCount)
 	{
-		super(program);
-		
-		imgbuf = BufferHelper.createFloatBuffer(FLOATS_PER_IMG * images);
-		
-		vertex = new GLBuffer(GLEnumBufferTarget.GL_ARRAY_BUFFER, GLEnumDataUsage.GL_STREAM_DRAW, GLEnumDataType.GL_FLOAT, imgbuf);
+		super(p);
 		
 		zBuffer = false;
 		
-		vertex.addAttrib(0, 2, GLConst.GL_FLOAT, false, 16, 0);			//Position data
-		vertex.addAttrib(1, 2, GLConst.GL_FLOAT, false, 16, 8);			//Texture off
-		
-		vao.addVBO(vertex);
+		do
+		{
+			createLayer();
+			
+		}
+		while (layers.size() < layerCount);
 		
 	}
 	
-	public Canvas(GLProgram program, int images, IPopulator<Canvas> pop)
+	public Canvas(GLProgram program, int layers, IPopulator<Canvas> pop)
 	{
-		this(program, images);
+		this(program, layers);
 		
 		pop.populate(this);
-		
-	}
-	
-	@Override
-	protected boolean initiate(RenderContext rcon)
-	{
-		return this.program.attachShaders(rcon.get2DShaders()) > 0;
-	}
-	
-	@Override
-	protected void doRender(RenderContext rcon) throws RenderException
-	{
-		if (this.images > 0)
-		{
-			GL1.glDrawArrays(GLEnumDrawType.GL_TRIANGLES, 0, this.images * 6);
-			
-		}
-		
-	}
-	
-	@Override
-	public void preRender(RenderContext rcon, double delta)
-	{
-		super.preRender(rcon, delta);
-		
-		if (this.imgbuf.position() != 0)
-		{
-			this.imgbuf.position(0);
-			
-		}
-		
-		if (this.updateImgBuf)
-		{
-			if (this.expanded)
-			{
-				this.vertex.uploadBuffer(this.imgbuf);
-				
-				this.expanded = false;
-				
-			}
-			else
-			{
-				this.vertex.updateVBO(this.imgbuf, 0);
-				
-			}
-			
-			this.updateImgBuf = false;
-			
-		}
 		
 	}
 	
 	@Override
 	public void postRender(RenderContext rcon)
 	{
-		super.postRender(rcon);
+		this.layers.forEach(((layer) -> {layer.postRender(rcon);}));
+		
+	}
+	
+	@Override
+	protected boolean initiate(RenderContext rcon)
+	{
+		if (this.program.shaderCount() == 0)
+		{
+			return this.program.attachShaders(rcon.get2DShaders()) > 0;
+		}
+		
+		return true;
+	}
+	
+	@Override
+	protected boolean doRender(RenderContext rcon) throws RenderException
+	{
+		boolean ret = false;
+		
+		for (int c = 0; c < this.layers.size(); c++)
+		{
+			if (this.layers.get(c).render(rcon) && !ret)
+			{
+				ret = true;
+				
+			}
+			
+		}
+		
+		return ret;
+	}
+	
+	@Override
+	public void setMaterial(Material mat)
+	{
+		this.currentLayer.setMaterial(mat);
+		
+	}
+	
+	@Override
+	public void preRender(RenderContext rcon)
+	{
+		this.layers.forEach(((layer) -> {layer.preRender(rcon);}));
 		
 	}
 	
@@ -165,17 +173,22 @@ public class Canvas extends RenderableObj
 	@Override
 	public RenderableObj setEnableZBuffer(boolean z)
 	{
-		if (z)
-		{
-			throw new UnsupportedOperationException("One does not simply enable Z buffering for a 2D canvas");
-		}
-		
-		return this;
+		throw new UnsupportedOperationException("Canvases cannot change their Z buffering status, you nincompoop.");
 	}
 	
 	public int getImageCount()
 	{
-		return this.images;
+		return this.currentLayer.getImageCount();
+	}
+	
+	public int getLayerCount()
+	{
+		return this.layers.size();
+	}
+	
+	public boolean doCoordCorrection()
+	{
+		return this.correctCoords;
 	}
 	
 	public void setSubCanvas(float xmin, float ymin, float xmax, float ymax)
@@ -186,15 +199,58 @@ public class Canvas extends RenderableObj
 	
 	public void setSubCanvas(Rectangle r)
 	{
-		if (this.sub == null)
+		this.currentLayer.setSubCanvas(r);
+		
+	}
+	
+	public Canvas setEnableCoordCorrection(boolean b)
+	{
+		this.correctCoords = b;
+		
+		return this;
+	}
+	
+	public void createLayer()
+	{
+		this.currentLayer = new CanvasLayer(this);
+		
+		this.layers.add(this.currentLayer);
+		
+	}
+	
+	public boolean nextLayer()
+	{
+		if (this.layers.hasNext())
 		{
-			synchronized (this)
-			{
-				this.sub = r;
-				
-			}
+			this.currentLayer = this.layers.next();
 			
+			return true;
 		}
+		
+		return false;
+	}
+	
+	public void setLayer(int layer)
+	{
+		this.currentLayer = this.layers.get(layer);
+		
+	}
+	
+	public void clear()
+	{
+		this.currentLayer = this.layers.get(0);
+		
+		this.layers.forEach(((layer) ->
+		{
+			layer.clear();
+			
+		}));
+		
+	}
+	
+	public void clearCurrentLayer()
+	{
+		this.currentLayer.clear();
 		
 	}
 	
@@ -215,41 +271,7 @@ public class Canvas extends RenderableObj
 	
 	public int drawImage(Rectangle r, Icon icon)
 	{
-		int pos = this.images * FLOATS_PER_IMG;
-		
-		if (this.imgbuf.position() != pos)
-		{
-			synchronized (this)
-			{
-				this.imgbuf.position(pos);
-				
-			}
-			
-		}
-		
-		if (this.imgbuf.remaining() == 0)
-		{
-			FloatBuffer fb = BufferHelper.expand(this.imgbuf, FLOATS_PER_IMG * 12);
-			
-			synchronized (this)
-			{
-				this.imgbuf = fb;
-				this.expanded = true;
-				
-			}
-			
-		}
-		
-		synchronized (this)
-		{
-			this.addCorners(r, icon);
-			
-			this.images++;
-			this.updateImgBuf = true;
-			
-		}
-		
-		return this.images - 1;
+		return this.currentLayer.drawImage(r, icon);
 	}
 	
 	public void redrawImage(int image, float x, float y, float z, float w, Icon icon)
@@ -260,76 +282,7 @@ public class Canvas extends RenderableObj
 	
 	public void redrawImage(int image, Rectangle r, Icon icon)
 	{
-		assert this.images > 0;
-		assert MathHelper.bounds(image, 0, this.images - 1);
-		
-		synchronized (this)
-		{
-			this.imgbuf.position(image * FLOATS_PER_IMG);
-			
-			this.addCorners(r, icon);
-			
-			this.imgbuf.position(0);
-			
-			this.updateImgBuf = true;
-			
-		}
-		
-	}
-	
-	public void clear()
-	{
-		this.imgbuf.clear();
-		this.images = 0;
-		
-		this.vertex.updateVBO(this.imgbuf, 0);
-		
-	}
-	
-	public Canvas setEnableCoordCorrection(boolean correct)
-	{
-		this.correctCoords = correct;
-		
-		return this;
-	}
-	
-	private void addCorners(Rectangle r, Icon icon)
-	{
-		if (this.sub != null)
-		{
-			r = this.sub.interpolate(r);
-			
-		}
-		
-		if (icon == null)
-		{
-			icon = Icon.BLANK_ICON;
-			
-		}
-		
-		this.addCorner(r.x, r.y, icon, 0);
-		this.addCorner(r.x, r.w, icon, 1);
-		this.addCorner(r.z, r.y, icon, 2);
-		
-		this.addCorner(r.x, r.w, icon, 1);
-		this.addCorner(r.z, r.y, icon, 2);
-		this.addCorner(r.z, r.w, icon, 3);
-		
-	}
-	
-	private void addCorner(float x, float y, Icon icon, int corner)
-	{
-		if (this.correctCoords)
-		{
-			x = (x * 2 - 1);
-			y = ((1 - y) * 2 - 1);
-			
-		}
-		
-		this.imgbuf.put(x);
-		this.imgbuf.put(y);
-		this.imgbuf.put(icon.getCorner(corner)[0]);
-		this.imgbuf.put(icon.getCorner(corner)[1]);
+		this.currentLayer.redrawImage(image, r, icon);
 		
 	}
 	
--- src/com/elusivehawk/caelum/render/CanvasLayer.java
+++ src/com/elusivehawk/caelum/render/CanvasLayer.java
@@ -1,6 +1,21 @@
 
 package com.elusivehawk.caelum.render;
 
+import java.nio.FloatBuffer;
+import com.elusivehawk.caelum.prefab.Rectangle;
+import com.elusivehawk.caelum.render.gl.GL1;
+import com.elusivehawk.caelum.render.gl.GLBuffer;
+import com.elusivehawk.caelum.render.gl.GLConst;
+import com.elusivehawk.caelum.render.gl.GLEnumBufferTarget;
+import com.elusivehawk.caelum.render.gl.GLEnumDataType;
+import com.elusivehawk.caelum.render.gl.GLEnumDataUsage;
+import com.elusivehawk.caelum.render.gl.GLEnumDrawType;
+import com.elusivehawk.caelum.render.gl.GLVertexArray;
+import com.elusivehawk.caelum.render.tex.Material;
+import com.elusivehawk.util.math.MathHelper;
+import com.elusivehawk.util.storage.BufferHelper;
+import com.elusivehawk.util.storage.DirtableStorage;
+
 /**
  * 
  * 
@@ -9,21 +24,218 @@ package com.elusivehawk.caelum.render;
  */
 public class CanvasLayer implements IRenderable
 {
-	@Override
-	public void preRender(RenderContext rcon, double delta)
+	private final Canvas parent;
+	private final GLBuffer vertex;
+	
+	private final DirtableStorage<Material> mat = new DirtableStorage<Material>(null).setSync();
+	private final GLVertexArray vao = new GLVertexArray();
+	
+	private FloatBuffer imgbuf = BufferHelper.createFloatBuffer(Canvas.FLOATS_PER_IMG * 12);
+	private Rectangle sub = null;
+	private int images = 0;
+	
+	private boolean expanded = false, updateImgBuf = false;
+	
+	@SuppressWarnings("unqualified-field-access")
+	public CanvasLayer(Canvas cvs)
 	{
+		parent = cvs;
+		vertex = new GLBuffer(GLEnumBufferTarget.GL_ARRAY_BUFFER, GLEnumDataUsage.GL_STREAM_DRAW, GLEnumDataType.GL_FLOAT, imgbuf);
+		
+		vertex.addAttrib(0, 2, GLConst.GL_FLOAT, false, 16, 0);		//Position data
+		vertex.addAttrib(1, 2, GLConst.GL_FLOAT, false, 16, 8);		//Texture off
+		
+		vao.addVBO(vertex);
 		
 	}
-
+	
 	@Override
-	public void postRender(RenderContext rcon)
+	public boolean render(RenderContext rcon) throws RenderException
 	{
+		if (this.images == 0)
+		{
+			return false;
+		}
+		
+		if (!this.vao.bind(rcon))
+		{
+			return false;
+		}
+		
+		if (!this.mat.isNull())
+		{
+			this.mat.get().bind(rcon);
+			
+		}
+		
+		GL1.glDrawArrays(GLEnumDrawType.GL_TRIANGLES, 0, this.images * 6);
+		
+		this.vao.unbind(rcon);
+		
+		if (!this.mat.isNull())
+		{
+			this.mat.get().unbind(rcon);
+			
+		}
+		
+		return true;
+	}
+	
+	@Override
+	public void preRender(RenderContext rcon)
+	{
+		if (!this.mat.isNull())
+		{
+			this.mat.get().preRender(rcon);
+			
+		}
+		
+		if (this.imgbuf.position() != 0)
+		{
+			this.imgbuf.position(0);
+			
+		}
+		
+		if (this.updateImgBuf)
+		{
+			if (this.expanded)
+			{
+				this.vertex.uploadBuffer(this.imgbuf);
+				
+				this.expanded = false;
+				
+			}
+			else
+			{
+				this.vertex.updateVBO(this.imgbuf, 0);
+				
+			}
+			
+			this.updateImgBuf = false;
+			
+		}
 		
 	}
-
+	
 	@Override
-	public void render(RenderContext rcon) throws RenderException
+	public void postRender(RenderContext rcon) throws RenderException
 	{
+		if (!this.mat.isNull())
+		{
+			this.mat.get().postRender(rcon);
+			
+		}
+		
+	}
+	
+	public int getImageCount()
+	{
+		return this.images;
+	}
+	
+	public void setMaterial(Material m)
+	{
+		this.mat.set(m);
+		
+	}
+	
+	public void setSubCanvas(Rectangle r)
+	{
+		this.sub = r;
+		
+	}
+	
+	public int drawImage(Rectangle r, Icon icon)
+	{
+		int pos = this.images * Canvas.FLOATS_PER_IMG;
+		
+		if (this.imgbuf.position() != pos)
+		{
+			this.imgbuf.position(pos);
+			
+		}
+		
+		if (this.imgbuf.remaining() == 0)
+		{
+			this.imgbuf = BufferHelper.expand(this.imgbuf, Canvas.FLOATS_PER_IMG * 12);
+			this.expanded = true;
+			
+		}
+		
+		this.addCorners(r, icon);
+		
+		this.images++;
+		this.updateImgBuf = true;
+		
+		return this.images - 1;
+	}
+	
+	public void redrawImage(int image, Rectangle r, Icon icon)
+	{
+		assert this.images > 0;
+		assert MathHelper.bounds(image, 0, this.images - 1);
+		
+		synchronized (this)
+		{
+			this.imgbuf.position(image * Canvas.FLOATS_PER_IMG);
+			
+			this.addCorners(r, icon);
+			
+			this.imgbuf.position(0);
+			
+			this.updateImgBuf = true;
+			
+		}
+		
+	}
+	
+	public void clear()
+	{
+		this.imgbuf.clear();
+		this.images = 0;
+		this.sub = null;
+		
+		this.vertex.updateVBO(this.imgbuf, 0);
+		
+	}
+	
+	private void addCorners(Rectangle r, Icon icon)
+	{
+		if (this.sub != null)
+		{
+			r = this.sub.interpolate(r);
+			
+		}
+		
+		if (icon == null)
+		{
+			icon = Icon.BLANK_ICON;
+			
+		}
+		
+		this.addCorner(r.x, r.y, icon, 0);
+		this.addCorner(r.z, r.y, icon, 1);
+		this.addCorner(r.x, r.w, icon, 2);
+		
+		this.addCorner(r.z, r.y, icon, 1);
+		this.addCorner(r.x, r.w, icon, 2);
+		this.addCorner(r.z, r.w, icon, 3);
+		
+	}
+	
+	private void addCorner(float x, float y, Icon icon, int corner)
+	{
+		if (this.parent == null || this.parent.doCoordCorrection())
+		{
+			x = (x * 2 - 1);
+			y = ((1 - y) * 2 - 1);
+			
+		}
+		
+		this.imgbuf.put(x);
+		this.imgbuf.put(y);
+		this.imgbuf.put(icon.getX(corner));
+		this.imgbuf.put(icon.getY(corner));
 		
 	}
 	
