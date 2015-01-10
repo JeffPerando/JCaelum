1e8d94e50287a028682d084093c012f05d34d9fd
Elusivehawk <be_the_trade@yahoo.com>
2015-01-09 22:46:59 -0800

    Icon is now saner.

--- src/com/elusivehawk/caelum/render/Icon.java
+++ src/com/elusivehawk/caelum/render/Icon.java
@@ -1,7 +1,9 @@
 
 package com.elusivehawk.caelum.render;
 
+import com.elusivehawk.caelum.CaelumException;
 import com.elusivehawk.caelum.prefab.Rectangle;
+import com.elusivehawk.util.parse.json.JsonArray;
 
 /**
  * 
@@ -14,7 +16,7 @@ public class Icon
 	public static final Icon BLANK_ICON = new Icon(0, 0, 1, 1);
 	
 	private final float[] stats;
-	private final float[][] corners;
+	private final float[] corners;
 	
 	public Icon(Rectangle r)
 	{
@@ -26,13 +28,46 @@ public class Icon
 	public Icon(float x, float y, float z, float w)
 	{
 		stats = new float[]{x, y, z, w};
-		corners = new float[][]{{x, y}, {x, w}, {z, y}, {z, w}};
+		corners = new float[]{x, y, z, y, x, w, z, w};
 		
 	}
 	
-	public float[] getCorner(int c)
+	@SuppressWarnings("unqualified-field-access")
+	public Icon(JsonArray json)
 	{
-		return this.corners[c];
+		if (json.length() < 4)
+		{
+			throw new ArrayIndexOutOfBoundsException("Too small of an array: Needs to be 4 or more");
+		}
+		
+		float[] fs = new float[4];
+		
+		for (int c = 0; c < 4; c++)
+		{
+			Object obj = json.getValue(c);
+			
+			if (!(obj instanceof Double))
+			{
+				throw new CaelumException("Found invalid input for icon in JSON array index %s: %s", c, obj);
+			}
+			
+			fs[c] = ((Double)obj).floatValue();
+			
+		}
+		
+		stats = fs;
+		corners = new float[]{fs[0], fs[1], fs[2], fs[1], fs[0], fs[3], fs[2], fs[3]};
+		
+	}
+	
+	public float getX(int corner)
+	{
+		return this.corners[2 * corner];
+	}
+	
+	public float getY(int corner)
+	{
+		return this.corners[2 * corner + 1];
 	}
 	
 	public float[] getRawCornerInfo()
